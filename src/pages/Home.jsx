3de0c87f07f8d2d449df7df189d8d5853c6880e3
unknown <you@example.comhttps://github.com/BBMARK/designshop.git1~>
2022-11-23 19:16:09 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Declare from "../components/Declare";
+import Slider from "../components/Slider";
+import Categories from "../components/Categories";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+
+
+
+
+const Home = () => {
+    return (
+    <div className="Container">
+        <Declare/>
+        <Navbar/>
+        <Slider/>
+        <Categories/>
+        <Products/>
+        <Newsletter/>
+        <Footer/>
+        
+    </div>
+    )
+}
+
+export default Home
